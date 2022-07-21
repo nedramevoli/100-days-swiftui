1bdb3edf1cf6455ae86f3d251fadaac243c59322
Nedra Mevoli ♥ <nedra.mevoli@willowtreeapps.com>
2022-07-21 14:46:47 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Nedra Mevoli on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
